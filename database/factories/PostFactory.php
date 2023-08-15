92d9a722ef5cab348abf69067f289af2740a2d2f
Ousmane Sylla <ousmanejrsylla@hotmail.com>
2023-08-15 16:03:03 +0000

    PostFactory and seed

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    { 
+        return [
+            'title' => fake()->sentence($nbWords = 6, $variableNbWords = true),
+            'contenu' => fake()->paragraph($nbSentences = 20, $variableNbSentences = true),
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+    }
+}
